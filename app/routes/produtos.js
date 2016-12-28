0c3c38140faba2d82e777c1441f0e8f451720827
Turbiani <leo.turbiani@gmail.com>
2016-12-28 16:58:10 -0200

    Inserindo validacao no back-end

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -26,7 +26,7 @@ module.exports = function(app){
     app.get('/produtos', listaProdutos);
 
     app.get('/produtos/form',function(req,res){
-        res.render('produtos/form', {errosValidacao:{}});
+        res.render('produtos/form', {errosValidacao:{}, produto:{}});
     });
 
     app.post('/produtos',function(req,res){
@@ -40,7 +40,14 @@ module.exports = function(app){
 
         var erros = req.validationErrors();
         if(erros){
-            res.render('produtos/form', {errosValidacao:erros});
+            res.format({
+                html: function(){
+                    res.status(400).render('produtos/form',{errosValidacao:erros,produto:produto});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            });
             return;
         }
 
--- cadastra-livros-terminal.js
+++ cadastra-livros-terminal.js
@@ -22,7 +22,7 @@ var client = http.request(configuracoes,function(res){
 });
 
 var produto = {
-    titulo : 'mais sobre node',
+    titulo : '',
     descricao: 'node, javascript e um pouco sobre http',
     preco: '100'
 };
