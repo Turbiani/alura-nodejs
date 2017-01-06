534a441d59d554801ba26101bbc16ebbda267fab
Turbiani <leo.turbiani@gmail.com>
2017-01-06 01:13:26 -0200

    Primeiro teste unitário

---
+++ test/produtos.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lcunha on 06/01/17.
+ */
+describe('#ProdutosController', function(){
+    it('#listagem json',function(){
+        console.log("teste de verificacao de listagem json");
+    });
+
+});
