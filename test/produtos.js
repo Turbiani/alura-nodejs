c855d8074b9f97b941df29ae7ab028b3a6681a50
Turbiani <leo.turbiani@gmail.com>
2017-01-07 17:11:47 -0200

    Usando biblioteca de assert do proprio NodeJS

--- test/produtos.js
+++ test/produtos.js
@@ -1,9 +1,25 @@
 /**
  * Created by lcunha on 06/01/17.
  */
+var http = require('http');
+var assert = require('assert');
 describe('#ProdutosController', function(){
-    it('#listagem json',function(){
-        console.log("teste de verificacao de listagem json");
+    it('#listagem json',function(done){
+        var configuracoes = {
+            hostname: 'localhost',
+            port:3000,
+            path:'/produtos',
+            headers: {
+                'Accept' : 'application/json'
+            }
+        };
+
+        http.get(configuracoes,function(res){
+            assert.equal(res.statusCode, 302);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+
+            done();
+        });
     });
 
 });
