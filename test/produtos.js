a861b8e61818f3940169be847e6a4a33a965f41d
Turbiani <leo.turbiani@gmail.com>
2017-01-07 18:32:07 -0200

    Trocando a biblioteca de testes para supertest, que já se integra com o Mocha e o express

--- test/produtos.js
+++ test/produtos.js
@@ -1,25 +1,15 @@
 /**
  * Created by lcunha on 06/01/17.
  */
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
+
 describe('#ProdutosController', function(){
     it('#listagem json',function(done){
-        var configuracoes = {
-            hostname: 'localhost',
-            port:3000,
-            path:'/produtos',
-            headers: {
-                'Accept' : 'application/json'
-            }
-        };
-
-        http.get(configuracoes,function(res){
-            assert.equal(res.statusCode, 302);
-            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-
-            done();
-        });
+        request.get('http://localhost:3000/produtos')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(302, done);
     });
 
 });
